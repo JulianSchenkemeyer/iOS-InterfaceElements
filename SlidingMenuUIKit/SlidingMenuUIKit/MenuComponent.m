19a664bb61dce928966885595a3cfd13deefa23c
Julian <Julian.schenkemeyer@gmail.com>
2015-01-21 19:12:58 +0100

    bl

---
+++ SlidingMenuUIKit/SlidingMenuUIKit/MenuComponent.m
@@ -0,0 +1,13 @@
+//
+//  menuComponent.m
+//  SlidingMenuUIKit
+//
+//  Created by Julian Schenkemeyer on 21/01/15.
+//  Copyright (c) 2015 SchenkemeyerJulian. All rights reserved.
+//
+
+#import "menuComponent.h"
+
+@implementation menuComponent
+
+@end
