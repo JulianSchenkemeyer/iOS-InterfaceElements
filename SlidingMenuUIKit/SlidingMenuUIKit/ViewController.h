caf2aebd843b0d711ea3b12b018c89a5da9b7396
Julian <Julian.schenkemeyer@gmail.com>
2015-01-21 15:35:45 +0100

    initial Commit for UIKit Version
    Another Version of the Sliding Menu, this time build with help of the
    UIKit Dynamics

---
+++ SlidingMenuUIKit/SlidingMenuUIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlidingMenuUIKit
+//
+//  Created by Julian Schenkemeyer on 21/01/15.
+//  Copyright (c) 2015 SchenkemeyerJulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
