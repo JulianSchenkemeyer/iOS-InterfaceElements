07e05275af92919579b32dd97c4d2cc3182bc67e
Julian <Julian.schenkemeyer@gmail.com>
2015-01-21 19:37:54 +0100

    Revert caf2aeb..c56c8a0
    This rolls back to commit caf2aebd843b0d711ea3b12b018c89a5da9b7396.

---
+++ SlidingMenuUIKit/SlidingMenuUIKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlidingMenuUIKit
+//
+//  Created by Julian Schenkemeyer on 21/01/15.
+//  Copyright (c) 2015 SchenkemeyerJulian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
